c72cbfb03fab6cc65a5f3334bceb70bc13417471
Thanh Minh <vmthanh24@gmail.com>
2022-03-03 17:30:17 +0700

    Initial project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Get.snackbar(
+                "This is snackbar",
+                "GetX Snackbar",
+                colorText: Colors.white,
+                snackPosition: SnackPosition.BOTTOM,
+                margin: EdgeInsets.all(16.0),
+                icon: Icon(
+                  Icons.check,
+                  color: Colors.greenAccent,
+                ),
+                backgroundColor: Colors.deepPurple[700],
+                mainButton: TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(color: Colors.white, width: 1.0),
+                    ),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: Text("Action"),
+                    ),
+                  ),
+                ),
+              );
+            },
+            child: Text("Show Snackbar"),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/subjects_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SubjectsPage extends StatelessWidget {
+  final String subject;
+
+  SubjectsPage({required this.subject });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
