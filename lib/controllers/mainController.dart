5c94f6ead4312b70b6366082d84e9edf6844c5d4
Thanh Minh <vmthanh24@gmail.com>
2022-03-05 20:25:42 +0700

    Using GetX workers

--- lib/controllers/mainController.dart
+++ lib/controllers/mainController.dart
@@ -5,15 +5,38 @@ class MainController extends GetxController {
   var isLoggedIn = false.obs;
   var followers = 0.obs;
 
+  // GetX Workers
+  @override
+  void onInit() {
+    // ever(isLoggedIn, (_){
+    //   print("Ever worker: called everytime when a value of a variable changes");
+    // },);
+
+    // everAll([isLoggedIn, followers], (_){
+    //   print("EverAll worker: called everytime when value of variable in the list changes");
+    // });
+
+    // once(isLoggedIn, (_){
+    //   print("Once worker: function called only once when the value changes");
+    // });
+
+    debounce(isLoggedIn, (_){
+      print("Debounce worker: function is called everytime user stops typing for the specific duration."
+          "\nGenerally used in search functionality as we don't want fetch the api every key stroke.");
+    }, time: Duration(seconds: 1));
+
+    super.onInit();
+  }
+
   // GetX method
-  updateStatus(bool newStatus){
+  updateStatus(bool newStatus) {
     isLoggedIn.value = newStatus;
   }
 
-  // GetBuilder method
-  // updateStatus(bool status){
-  //   isLoggedIn = status;
-  //   // update();
-  //   update(['login_status_widget', 'followers_widget']);
-  // }
+// GetBuilder method
+// updateStatus(bool status){
+//   isLoggedIn = status;
+//   // update();
+//   update(['login_status_widget', 'followers_widget']);
+// }
 }
